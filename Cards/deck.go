5b0820c1bb801ba73559c979fa4e0e73510d2a29
DrizinCoder <guiaulas2020@gmail.com>
2025-08-15 15:42:34 -0300

    :sparkles:feat: Create_Card, NewStock, GenerateID methods creation

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,5 +1,34 @@
-package Cards
+package main
 
-type Deck struct {
-	Stock []*Card
+import "sync"
+
+type Stock struct {
+	mu   sync.Mutex
+	Deck []*Card
+}
+
+var nextID int
+var muID sync.Mutex
+
+func generateID() int {
+	muID.Lock()
+	defer muID.Unlock()
+	nextID++
+	return nextID
+}
+
+func NewStock() *Stock {
+	return &Stock{
+		Deck: make([]*Card, 0),
+	}
+}
+
+func (s *Stock) CreateCard(name string, power int, life int, rarity Rare) *Card {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	card := New_Card(generateID(), name, power, life, rarity)
+	s.Deck = append(s.Deck, card)
+
+	return card
 }
